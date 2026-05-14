b0536e346cd75181544989db2608419e51a5b094
Shazeb <shazebshaikh989@gmail.com>
2026-05-15 02:19:01 +0530

    remove node_modules from tracking

--- frontend/.next/static/chunks/app/about/page-f2c871c09f84e0e6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{2603:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4159,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[159,347,239,358],()=>s(2603)),_N_E=e.O()}]);
